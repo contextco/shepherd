9b2ec14c7bd8e3c8e0a03d4f9b0c601557f68a3c
alecbarber <alec@context.ai>
2024-10-22 15:21:48 +0100

    more gems and amniauth controller

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -5,4 +5,41 @@ class User < ApplicationRecord
          :rememberable,
          :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  class << self
+    def from_omniauth!(access_token)
+      data = access_token.info
+      user = User.where(email: data["email"]).first
+
+      if user
+        user.update!(
+          name: data["name"],
+          first_name: data["first_name"],
+          last_name: data["last_name"],
+          profile_picture_url: data["image"]
+        )
+        return user
+      end
+
+      ActiveRecord::Base.transaction do
+        user = create_user_from_omniauth(data)
+      end
+
+      user
+    end
+
+    private
+
+    def create_user_from_omniauth(data)
+      User.create!(
+        name: data["name"],
+        email: data["email"],
+        first_name: data["first_name"],
+        last_name: data["last_name"],
+        profile_picture_url: data["image"],
+        password: Devise.friendly_token[0, 20],
+        credits: User::STARTING_CREDITS,
+        )
+    end
+  end
 end
