d479461ff90d4a283de5b9177fb2107ce780122c
alecbarber <alec@context.ai>
2024-10-22 16:06:47 +0100

    add new fields to users table. add working amni auth tests.

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_22_144055) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_22_150544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_22_144055) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "profile_picture_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
