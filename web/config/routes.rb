45263c4fc07a6dcc008be9252e2b1e4b78c8bf1c
alecbarber <alec@context.ai>
2024-10-22 15:09:59 +0100

    omni auth and other bits

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,14 +13,14 @@ Rails.application.routes.draw do
 
   root "dashboard#index"
 
-  # devise_for :users,
-  #            controllers: { omniauth_callbacks: "users/omniauth_callbacks" },
-  #            skip: [ :sessions, :registrations ]
-  #
-  # # as :user do
-  # devise_scope :user do
-  #   get "/users", to: "devise/sessions#new", as: :new_user_session
-  #   post "/users/sign_in", to: "devise/sessions#create", as: :user_session
-  #   delete "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
-  # end
+  devise_for :users,
+             controllers: { omniauth_callbacks: "users/omniauth_callbacks" },
+             skip: [ :sessions, :registrations ]
+
+  # as :user do
+  devise_scope :user do
+    get "/users", to: "devise/sessions#new", as: :new_user_session
+    post "/users/sign_in", to: "devise/sessions#create", as: :user_session
+    delete "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
 end
