b54d95f76e3dab2eda05bc19efc4dad7d8c2c698
alecbarber <alec@context.ai>
2024-10-22 15:09:59 +0100

    omni auth and other bits

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :rememberable,
+         :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 end
