d1cffe05e2d3f9296fadbe7079b5dac07c7220f9
alecbarber <alec@context.ai>
2024-10-22 15:06:22 +0100

    add user stuff

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,6 +10,18 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :dashboard, only: [ :index ]
+
+  root "dashboard#index"
+
+  # devise_for :users,
+  #            controllers: { omniauth_callbacks: "users/omniauth_callbacks" },
+  #            skip: [ :sessions, :registrations ]
+  #
+  # # as :user do
+  # devise_scope :user do
+  #   get "/users", to: "devise/sessions#new", as: :new_user_session
+  #   post "/users/sign_in", to: "devise/sessions#create", as: :user_session
+  #   delete "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  # end
 end
