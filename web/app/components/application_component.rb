2c17c46fc47794ddd6aab6fa4c9a8db5bbbb8a72
alecbarber <alec@context.ai>
2024-10-23 11:17:52 +0100

    Basic ui

---
+++ web/app/components/application_component.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class ApplicationComponent < ViewComponent::Base
+  include ActiveModel::Attributes
+  include ActiveModel::AttributeAssignment
+  include RailsHeroicon::Helper
+
+  delegate :current_user, :current_team, :current_project, :user_signed_in?, to: :helpers
+
+  def initialize(**args)
+    super
+    assign_attributes(args)
+  end
+end
---
+++ web/app/components/burger_menu_content_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class BurgerMenuContentComponent < ViewComponent::Base
+  renders_one :burger_menu_content
+end
