ec0382b240ca5d0e8815932a961b95ef7f51e650
alecbarber <alec@context.ai>
2024-10-23 11:17:52 +0100

    Basic ui

---
+++ web/app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+
+class UserController < ApplicationController
+
+  def index
+    redirect_to root_path if current_user.nil?
+  end
+
+  def leave_team
+    team = current_user.team
+    current_user.update!(team: nil)
+    team.destroy! if team.users.empty?
+
+    redirect_to team_index_path
+  end
+end
