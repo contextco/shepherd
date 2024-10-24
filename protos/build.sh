79bcee2700029d056cd3bf67cc134a3ab5d0d96a
Alex Gamble <agamble.uk@gmail.com>
2024-10-24 10:40:23 +0100

    Add golang grpc client

--- protos/build.sh
+++ protos/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 grpc_tools_ruby_protoc -I ./protos --ruby_out=web/lib/generated/protos --grpc_out=web/lib/generated/grpc ./protos/service.proto
+protoc --go_out=./client/generated --go_opt=paths=source_relative --go-grpc_out=./client/generated --go-grpc_opt=paths=source_relative ./protos/service.proto
