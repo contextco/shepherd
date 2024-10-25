6ab496369c1f943853329538a094185ca9b683fd
alecbarber <alec@context.ai>
2024-10-25 14:50:39 +0100

    fix

--- web/config/initializers/gruf.rb
+++ web/config/initializers/gruf.rb
@@ -1,8 +1,8 @@
 
 Gruf.configure do |c|
   c.server_binding_url = "0.0.0.0:50051"
-  c.backtrace_on_error = true #!Rails.env.production?
-  c.use_exception_message = true #!Rails.env.production?
+  c.backtrace_on_error = !Rails.env.production?
+  c.use_exception_message = !Rails.env.production?
 
   # Add authentication interceptor if needed
   # c.interceptors.use(
