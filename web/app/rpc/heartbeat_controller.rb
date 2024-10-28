ac661d4d4ff2fdf8bfe575abd85b2a1a676640dc
Alex Gamble <agamble.uk@gmail.com>
2024-10-28 14:31:09 +0000

    Record heartbeats as health logs when a request is received

--- web/app/rpc/heartbeat_controller.rb
+++ web/app/rpc/heartbeat_controller.rb
@@ -3,6 +3,17 @@ class HeartbeatController < RpcController
   bind OnPrem::Service
 
   def heartbeat
+    record_heartbeat
+
     HeartbeatResponse.new
+  end
+
+  private
+
+  def record_heartbeat
+    current_deployment.transaction do
+      container = current_deployment.containers.find_or_create_by!(name: request.message.identity.name)
+      container.health_logs.create!(lifecycle_id: request.message.identity.lifecycle_id)
+    end
   end
 end
---
+++ web/spec/models/health_log_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe HealthLog, type: :model do
+end
