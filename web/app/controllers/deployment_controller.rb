4a1df40e280195e71a2f02d14c343f0e93d92ea3
alecbarber <alec@context.ai>
2024-10-29 11:19:23 +0000

    Basic tokens ui for deployments.

--- web/app/controllers/deployment_controller.rb
+++ web/app/controllers/deployment_controller.rb
@@ -29,6 +29,10 @@ class DeploymentController < ApplicationController
     @deployment = current_team.deployments.find(params[:id])
   end
 
+  def tokens
+    @deployment = current_team.deployments.find(params[:id])
+  end
+
   private
 
   def deployment_params
