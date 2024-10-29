ae49f0db7c813597f218edce0e0da5ae2090efd0
alecbarber <alec@context.ai>
2024-10-29 11:29:08 +0000

    Fix resource in wrong sub resource.
    Changed tokens deployments page to index of tokens sub resource instead.

--- web/app/controllers/deployment_token_controller.rb
+++ web/app/controllers/deployment_token_controller.rb
@@ -1,22 +1,25 @@
 
 class DeploymentTokenController < ApplicationController
+  def index
+    @deployment = current_team.deployments.find(params[:deployment_id])
+  end
   def destroy
     deployment.tokens.find(params[:id]).destroy!
 
     flash[:notice] = "Token deleted"
-    redirect_to tokens_deployment_path(deployment)
+    redirect_to deployment_token_index_path(deployment)
   end
 
   def create
     deployment.tokens.create!
 
     flash[:notice] = "Token created"
-    redirect_to tokens_deployment_path(deployment)
+    redirect_to deployment_token_index_path(deployment)
   end
 
   private
 
   def deployment
-    @deployment ||= Deployment.find(params[:deployment_container_id])
+    @deployment ||= Deployment.find(params[:deployment_id])
   end
 end
