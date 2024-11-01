657e30a3828427acebb9434d3ac0caae44f0dd57
Alex Gamble <agamble.uk@gmail.com>
2024-11-01 17:51:29 +0000

    Install from golang binary now works

--- sidecar/main.go
+++ sidecar/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +12,12 @@ import (
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
+const releaseName = "sidecar"
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	chart, err := createChart()
 	if err != nil {
 		log.Fatalf("Error creating chart: %v", err)
@@ -19,7 +25,12 @@ func main() {
 
 	fmt.Println(chart.Validate())
 
-	err = installChart(chart)
+	err = uninstallChart()
+	if err != nil {
+		log.Fatalf("Error uninstalling chart: %v", err)
+	}
+
+	err = installChart(ctx, chart)
 	if err != nil {
 		log.Fatalf("Error installing chart: %v", err)
 	}
@@ -39,7 +50,25 @@ func NewSettings() *settings {
 	}
 }
 
-func installChart(chart *chart.Chart) error {
+func uninstallChart() error {
+	s := NewSettings()
+	actionConfig := new(action.Configuration)
+	if err := actionConfig.Init(s.RESTClientGetter(), "default", os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
+		return fmt.Errorf("failed to initialize helm configuration: %w", err)
+	}
+
+	client := action.NewUninstall(actionConfig)
+	client.IgnoreNotFound = true
+
+	_, err := client.Run(releaseName)
+	if err != nil {
+		return fmt.Errorf("failed to uninstall chart: %w", err)
+	}
+
+	return nil
+}
+
+func installChart(ctx context.Context, chart *chart.Chart) error {
 	s := NewSettings()
 	actionConfig := new(action.Configuration)
 	if err := actionConfig.Init(s.RESTClientGetter(), "default", os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
@@ -48,9 +77,10 @@ func installChart(chart *chart.Chart) error {
 
 	client := action.NewInstall(actionConfig)
 	client.Namespace = "default"
-	client.ReleaseName = "my-release"
+	client.ReleaseName = releaseName
+	client.Replace = true
 
-	rel, err := client.Run(chart, nil)
+	rel, err := client.RunWithContext(ctx, chart, nil)
 	if err != nil {
 		return fmt.Errorf("failed to install chart: %w", err)
 	}
@@ -63,6 +93,10 @@ func createChart() (*chart.Chart, error) {
 	files, err := TemplateFiles()
 	if err != nil {
 		return nil, err
+	}
+
+	for _, file := range files {
+		fmt.Printf("file: %s\n", file.Name)
 	}
 
 	chart, err := loader.LoadFiles(files)
--- sidecar/template.go
+++ sidecar/template.go
@@ -3,31 +3,40 @@ package main
 import (
 	"embed"
 	"fmt"
+	"io/fs"
 	"path/filepath"
 
 	"helm.sh/helm/v3/pkg/chart/loader"
 )
 
-//go:embed templates
+//go:embed all:templates
 var template embed.FS
 
 func TemplateFiles() ([]*loader.BufferedFile, error) {
-	files, err := template.ReadDir("templates")
-	if err != nil {
-		return nil, fmt.Errorf("error reading templates directory: %w", err)
-	}
-
 	bufferedFiles := []*loader.BufferedFile{}
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		content, err := template.ReadFile(filepath.Join("templates", file.Name()))
+	err := fs.WalkDir(template, "templates", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			return nil, fmt.Errorf("error reading template file %s: %w", file.Name(), err)
+			return err
+		}
+		if d.IsDir() {
+			return nil
 		}
 
-		bufferedFiles = append(bufferedFiles, &loader.BufferedFile{Name: file.Name(), Data: content})
+		content, err := template.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("error reading template file %s: %w", path, err)
+		}
+
+		name, err := filepath.Rel("templates", path)
+		if err != nil {
+			return fmt.Errorf("error getting relative path for %s: %w", path, err)
+		}
+
+		bufferedFiles = append(bufferedFiles, &loader.BufferedFile{Name: name, Data: content})
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error walking templates directory: %w", err)
 	}
 
 	return bufferedFiles, nil
