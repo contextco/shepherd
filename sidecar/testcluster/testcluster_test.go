0632592471462fd7d2670fd335ea4a8355b42f00
Alex Gamble <agamble.uk@gmail.com>
2024-11-04 12:21:29 +0000

    Add a way to spin up and down test k8s clusters

---
+++ sidecar/testcluster/testcluster_test.go
@@ -0,0 +1,63 @@
+package testcluster
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	cluster := New(t, ctx, "test-cluster")
+	if cluster == nil {
+		t.Fatal("expected cluster to not be nil")
+	}
+
+	if cluster.name != "test-cluster" {
+		t.Errorf("expected cluster name to be test-cluster, got %s", cluster.name)
+	}
+
+	if cluster.KubeConfig == nil {
+		t.Error("expected kubeconfig file to not be nil")
+	}
+}
+
+func TestClusterOperations(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	cluster := &Cluster{
+		name: "test-ops-cluster",
+	}
+
+	// Test create
+	if err := cluster.create(ctx); err != nil {
+		t.Fatalf("failed to create cluster: %v", err)
+	}
+
+	// Test getting kubeconfig
+	config, err := cluster.getKubeConfig(ctx)
+	if err != nil {
+		t.Fatalf("failed to get kubeconfig: %v", err)
+	}
+	if config == "" {
+		t.Error("expected non-empty kubeconfig")
+	}
+
+	// Test writing kubeconfig
+	kubeConfigFile, err := cluster.writeTempKubeConfig(ctx)
+	if err != nil {
+		t.Fatalf("failed to write kubeconfig: %v", err)
+	}
+	if kubeConfigFile == nil {
+		t.Error("expected kubeconfig file to not be nil")
+	}
+	defer kubeConfigFile.Close()
+
+	// Test delete
+	if err := cluster.delete(ctx); err != nil {
+		t.Fatalf("failed to delete cluster: %v", err)
+	}
+}
