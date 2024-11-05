49fa9addf1f19f4fb880af19281d04ab803153f8
alecbarber <alec@context.ai>
2024-11-05 11:11:32 +0000

    Refactors and make the edit pages make sense and scoped correctly.
    Former-commit-id: 140b56c848c9defebf320e715c33fa1400c60206

---
+++ web/app/components/applications/edit_application_form_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Applications::EditApplicationFormComponent < ApplicationComponent
+  attribute :application
+end
---
+++ web/app/components/services/element_component.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Services::ElementComponent < ApplicationComponent
+  attribute :service
+end
