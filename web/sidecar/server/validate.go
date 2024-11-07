b9d33d4a7b5a6c55bcea8749a378c75953314e64
Alex Gamble <agamble.uk@gmail.com>
2024-11-07 17:53:24 +0000

    Tweak validation slightly and add secret
    Former-commit-id: 022654c57e0c94f1e2d40e9f0d599093942cda76

--- web/sidecar/server/validate.go
+++ web/sidecar/server/validate.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 	"sidecar/chart"
 	sidecar_pb "sidecar/generated/sidecar_pb"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) ValidateChart(ctx context.Context, req *sidecar_pb.ValidateChartRequest) (*sidecar_pb.ValidateChartResponse, error) {
@@ -12,13 +15,18 @@ func (s *Server) ValidateChart(ctx context.Context, req *sidecar_pb.ValidateChar
 		return nil, errors.New("chart is required")
 	}
 
-	chart, err := chart.NewFromProto(req.GetChart())
+	c, err := chart.NewFromProto(req.GetChart())
 	if err != nil {
 		return nil, err
 	}
 
-	if err := chart.Validate(); err != nil {
-		return nil, err
+	if err := c.Validate(); err != nil && errors.Is(err, chart.ValidationError) {
+		return &sidecar_pb.ValidateChartResponse{
+			Valid:  false,
+			Errors: []string{err.Error()},
+		}, nil
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to validate chart: %s", err.Error())
 	}
 
 	return &sidecar_pb.ValidateChartResponse{
