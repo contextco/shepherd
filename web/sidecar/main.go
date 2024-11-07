a389b84fe07c19a3dad80bbcad0608e7df413fe3
Alex Gamble <agamble.uk@gmail.com>
2024-11-07 15:59:09 +0000

    Replace main with server
    Former-commit-id: 880411c3a1cdb5b22f6fed99636809509b8d8f43

--- web/sidecar/main.go
+++ web/sidecar/main.go
@@ -3,10 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"net/url"
-
-	sidecarchart "sidecar/chart"
-	"sidecar/repo"
 
 	_ "github.com/joho/godotenv/autoload" // load env vars
 )
@@ -22,31 +18,7 @@ func main() {
 		log.Fatalf("Error creating modules: %v", err)
 	}
 
-	chart, err := sidecarchart.Empty(releaseName)
-	if err != nil {
-		log.Fatalf("Error creating chart: %v", err)
-	}
-
-	chart, err = chart.ApplyParams(&sidecarchart.Params{
-		ChartName:    "sidecar",
-		ChartVersion: "0.2.0",
-
-		ReplicaCount: 1,
-		Environment: sidecarchart.Environment{
-			"FOO": "BAR",
-		},
-	})
-	if err != nil {
-		log.Fatalf("Error applying params: %v", err)
-	}
-
-	repo, err := repo.NewClient(ctx, modules.Store, &url.URL{}) // TODO: get base URL from env
-	if err != nil {
-		log.Fatalf("Error creating repo client: %v", err)
-	}
-
-	err = repo.Add(ctx, chart, "sidecar")
-	if err != nil {
-		log.Fatalf("Error adding chart to repo: %v", err)
+	if err := modules.Server.Run(ctx); err != nil {
+		log.Fatalf("Error running server: %v", err)
 	}
 }
