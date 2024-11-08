8be7c3dcd223b9213a5d9161a7627f1a4d75ac07
alecbarber <alec@context.ai>
2024-11-08 14:48:45 +0000

    update tests to use mocking

--- web/spec/controllers/helm/repo_controller_spec.rb
+++ web/spec/controllers/helm/repo_controller_spec.rb
@@ -3,6 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Helm::RepoController, type: :controller do
+  # this works locally without mocking but in test it does not
+  let(:mock_bucket) { instance_double("Google::Cloud::Storage::Bucket") }
+  let(:mock_file) { instance_double("Google::Cloud::Storage::File") }
+  let(:file_content) { "apiVersion: v1\nentries: {}" }
+
+  before do
+    allow(GCSClient).to receive(:onprem_bucket).and_return(mock_bucket)
+    allow(mock_bucket).to receive(:file).with("sidecar/index.yaml").and_return(mock_file)
+    allow(mock_file).to receive_message_chain(:download, :string).and_return(file_content)
+  end
+
   describe 'GET #index_yaml' do
     it 'returns a success response' do
       get :index_yaml
