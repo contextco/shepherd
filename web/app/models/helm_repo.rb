d84f0e522264047215a9ef6bd0617eb6723a041f
alecbarber <alec@context.ai>
2024-11-11 15:01:03 +0000

    added helpers in helm repo model for helm install commands
    Former-commit-id: e1a926a18d89c9fa8f99da1dbf96a026e0c072e0

--- web/app/models/helm_repo.rb
+++ web/app/models/helm_repo.rb
@@ -6,6 +6,26 @@ class HelmRepo < ApplicationRecord
   has_many :helm_users, dependent: :destroy
   validates :name, presence: true
 
+  def add_repo_command
+    # helm command to add repo
+    command = "helm repo add #{name} #{base_url}/#{name} --username #{helm_users.first.name} --password #{helm_users.first.password}"
+    command = "#{command} --insecure-skip-tls-verify" if Rails.env.development? || Rails.env.test?
+
+    command
+  end
+
+  def pull_chart_command(service_name:)
+    service = ProjectService.find_by(name: service_name)
+    # eventually this will only expose the main chart and not individual chart but for now we can expose any service
+    "helm pull #{name}/#{service.name} --untar"
+  end
+
+  def install_chart_command(service_name:)
+    service = ProjectService.find_by(name: service_name)
+
+    "helm install #{service.name} #{name}/#{service.name}"
+  end
+
   def valid_credentials?(name, password)
     helm_users.exists?(name:, password:)
   end
@@ -19,6 +39,13 @@ class HelmRepo < ApplicationRecord
   end
 
   private
+
+  def base_url
+    # TODO: this does not belong here
+    "http://localhost:3000" if Rails.env.development? || Rails.env.test?
+
+    "https://vpc.context.ai"
+  end
 
   def bucket
     @bucket ||= GCSClient.onprem_bucket
