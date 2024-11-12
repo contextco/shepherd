1557664978b48c8c5ace07819e0fc3c09f8ae0e8
Alex Gamble <agamble.uk@gmail.com>
2024-11-12 17:49:38 +0000

    Add additional fields
    Former-commit-id: b8750e0fe9c4f4bf07390117db6f34481c10028b

--- web/spec/factories/project_service_factory.rb
+++ web/spec/factories/project_service_factory.rb
@@ -2,9 +2,15 @@ FactoryBot.define do
   factory :project_service do
     name { 'generic-service' }
     image { 'generic-service:12.4.3' }
-    environment_variables { { 'key' => 'value' } }
+    environment_variables { [{ name: 'key', value: 'value' }] }
     secrets { [ 'my-secret' ] }
     cpu_cores { 1 }
     memory_bytes { 2.gigabytes }
+
+    transient do
+      team { nil }
+    end
+
+    project_version { association :project_version, **{ team: team }.compact_blank }
   end
 end
