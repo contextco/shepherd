e2cc4c018d69ba05ed0c7f05d91b219816b98cdb
alecbarber <alec@context.ai>
2024-11-19 14:42:42 +0000

    Basic ui for dependencies.

--- web/app/models/team.rb
+++ web/app/models/team.rb
@@ -8,6 +8,7 @@ class Team < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :project_versions, through: :projects
   has_many :services, dependent: :destroy, through: :project_versions, class_name: "ProjectService"
+  has_many :dependencies, dependent: :destroy, through: :project_versions, class_name: "Dependency"
 
   def setup_scaffolding!(name, description)
     transaction do
