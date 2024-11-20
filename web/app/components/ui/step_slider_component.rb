76de372448ac999f3907d0a7d199ddd46105d13f
alecbarber <alec@context.ai>
2024-11-20 11:30:29 +0000

    remove cols for resources in dependencies
    this all needs to go into the configs jsonb

--- web/app/components/ui/step_slider_component.rb
+++ web/app/components/ui/step_slider_component.rb
@@ -7,4 +7,10 @@ class UI::StepSliderComponent < ApplicationComponent
   attribute :name
 
   attribute :disabled_steps, default: []
+
+  def value
+    # return steps.find_index(form.object&.send(:name)) if form.object&.respond_to?(:name)
+    #
+    # steps.find_index(form.object&.configs&.fetch(:name, nil))
+  end
 end
