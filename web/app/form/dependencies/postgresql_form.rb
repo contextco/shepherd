1a139a33e33728090f9c03acc3cd7a216c35b0fa
Alex Gamble <agamble.uk@gmail.com>
2024-11-21 11:55:04 +0000

    Refactor to allow per service overrides of the CPU, RAM and disk space options for dependencies

--- web/app/form/dependencies/postgresql_form.rb
+++ web/app/form/dependencies/postgresql_form.rb
@@ -3,6 +3,18 @@
 class Dependencies::PostgresqlForm < Dependencies::Base
   validate :version_inclusion
 
+  CPU_CORES_OPTIONS = [
+    1, 2, 4, 8, 16, 32
+  ].freeze
+
+  MEMORY_OPTIONS = [
+    1.gigabyte, 2.gigabytes, 4.gigabytes, 8.gigabytes, 16.gigabytes, 32.gigabytes
+  ].freeze
+
+  DISK_OPTIONS = [
+    10.gigabytes, 20.gigabytes, 40.gigabytes, 80.gigabytes, 160.gigabytes, 320.gigabytes
+  ].freeze
+
   attribute :configs do
     attribute :db_name, default: "default_db_name"
     attribute :db_user, default: "default_db_username"
@@ -10,9 +22,9 @@ class Dependencies::PostgresqlForm < Dependencies::Base
     attribute :memory_bytes, :integer
     attribute :disk_bytes, :integer
 
-    validates :cpu_cores, presence: true, inclusion: { in: Services::CPUSliderComponent::OPTIONS }, numericality: { only_integer: true }
-    validates :memory_bytes, presence: true, inclusion: { in: Services::MemorySliderComponent::OPTIONS }, numericality: { only_integer: true }
-    validates :disk_bytes, presence: true, inclusion: { in: Services::DiskSliderComponent::OPTIONS }, numericality: { only_integer: true }
+    validates :cpu_cores, presence: true, inclusion: { in: CPU_CORES_OPTIONS }, numericality: { only_integer: true }
+    validates :memory_bytes, presence: true, inclusion: { in: MEMORY_OPTIONS }, numericality: { only_integer: true }
+    validates :disk_bytes, presence: true, inclusion: { in: DISK_OPTIONS }, numericality: { only_integer: true }
 
     validate :valid_postgresql_db_name
     validate :valid_postgresql_db_user
--- web/app/form/dependencies/redis_form.rb
+++ web/app/form/dependencies/redis_form.rb
@@ -3,6 +3,19 @@
 class Dependencies::RedisForm < Dependencies::Base
   validate :version_inclusion
 
+  CPU_CORES_OPTIONS = [
+    1, 2, 4, 8, 16, 32
+  ].freeze
+
+  MEMORY_OPTIONS = [
+    1.gigabyte, 2.gigabytes, 4.gigabytes, 8.gigabytes, 16.gigabytes, 32.gigabytes, 64.gigabytes
+  ].freeze
+
+  DISK_OPTIONS = [
+    10.gigabytes, 20.gigabytes, 40.gigabytes, 80.gigabytes, 160.gigabytes, 320.gigabytes
+  ].freeze
+
+
   attribute :configs do
     attribute :max_memory_policy
     attribute :cpu_cores, :integer
@@ -10,9 +23,9 @@ class Dependencies::RedisForm < Dependencies::Base
     attribute :disk_bytes, :integer
 
     validates :max_memory_policy, presence: true, inclusion: { in: Dependencies::RedisComponent::MAX_MEMORY_POLICY_OPTIONS.map(&:first) }
-    validates :cpu_cores, presence: true, inclusion: { in: Services::CPUSliderComponent::OPTIONS }, numericality: { only_integer: true }
-    validates :memory_bytes, presence: true, inclusion: { in: Services::MemorySliderComponent::OPTIONS }, numericality: { only_integer: true }
-    validates :disk_bytes, presence: true, inclusion: { in: Services::DiskSliderComponent::OPTIONS }, numericality: { only_integer: true }
+    validates :cpu_cores, presence: true, inclusion: { in: CPU_CORES_OPTIONS }, numericality: { only_integer: true }
+    validates :memory_bytes, presence: true, inclusion: { in: MEMORY_OPTIONS }, numericality: { only_integer: true }
+    validates :disk_bytes, presence: true, inclusion: { in: DISK_OPTIONS }, numericality: { only_integer: true }
   end
 
   def version_inclusion
