0092ba32fd755666ddf975ac638a18da88a5e365
Alex Gamble <agamble.uk@gmail.com>
2024-11-26 14:01:24 +0000

    Refactor tests
    Former-commit-id: 30d39c55cf66a2517babe2fbe5744e10a4a1e265

--- web/sidecar/server/server.go
+++ web/sidecar/server/server.go
@@ -7,15 +7,12 @@ import (
 	"log/slog"
 	"net"
 	"os"
-	"sidecar/chart"
 	"sidecar/repo"
 
 	sidecar_pb "sidecar/generated/sidecar_pb"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type Server struct {
@@ -29,23 +26,6 @@ func New(port string, repoClient *repo.Client) *Server {
 		port:       port,
 		repoClient: repoClient,
 	}
-}
-
-func (s *Server) PublishChart(ctx context.Context, req *sidecar_pb.PublishChartRequest) (*sidecar_pb.PublishChartResponse, error) {
-	if len(req.GetChart().GetServices()) == 0 {
-		return nil, status.Error(codes.InvalidArgument, "services are required")
-	}
-
-	chart, err := chart.NewFromProto(req.GetChart().GetName(), req.GetChart().GetVersion(), req.GetChart())
-	if err != nil {
-		return nil, err
-	}
-
-	if err := s.repoClient.Add(ctx, chart, req.RepositoryDirectory); err != nil {
-		return nil, err
-	}
-
-	return &sidecar_pb.PublishChartResponse{}, nil
 }
 
 func (s *Server) Run(ctx context.Context) error {
