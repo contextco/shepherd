6b82e92f59a7d84881c80385280c50abfb589e52
alecbarber <alec@context.ai>
2024-11-27 16:12:08 +0000

    Add docs page (empty) for docs with password protection

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_26_151357) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_27_122046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -125,6 +125,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_26_151357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "dummy", default: false, null: false
+    t.string "password"
     t.index ["project_id"], name: "index_project_subscribers_on_project_id"
   end
 
