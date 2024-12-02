efe1ea5f77a62e40097f4bc7b4a1b2a57c526335
alecbarber <alec@context.ai>
2024-12-02 10:46:56 +0000

    Edit form for dependencies.

--- web/app/form/dependencies/base.rb
+++ web/app/form/dependencies/base.rb
@@ -3,6 +3,8 @@
 class Dependencies::Base
   include FormObject
 
+  attribute :dependency_id
+
   attribute :name
   attribute :version
   attribute :repo_url
@@ -13,5 +15,23 @@ class Dependencies::Base
 
   def create_dependency(project_version)
     project_version.dependencies.create!(name:, version:, repo_url:, configs: configs_params)
+  end
+
+  def update_dependency(dependency)
+    dependency.update!(name:, version:, repo_url:, configs: configs_params)
+  end
+
+  def self.from_dependency(dependency)
+    # override as needed in child forms
+    f = Dependencies::RedisForm.new
+    f.assign_attributes(
+      dependency_id: dependency.id,
+      name: dependency.name,
+      version: dependency.version,
+      repo_url: dependency.repo_url,
+      configs: dependency.configs
+    )
+
+    f
   end
 end
