3a0acc3dbfe1680fb4575a676b30baee38e25d40
alecbarber <alec@context.ai>
2024-12-10 11:14:02 +0000

    return to storing raw bytes for disk persistency for redis and postresql.
    We now convert this in overrides modules to string.
    
    
    Former-commit-id: 51b9b9a3bfec336145e3e29aecf941bbf5e55cd6

--- web/app/lib/chart/override/base.rb
+++ web/app/lib/chart/override/base.rb
@@ -5,14 +5,24 @@ class Chart::Override::Base
 
   private
 
-  def convert_value(name, value, value_types)
-    case value_types[name.to_sym]
-    when :number
-      Google::Protobuf::Value.new(number_value: value)
-    when :string
-      Google::Protobuf::Value.new(string_value: value)
-    else
-      raise InvalidConfigError, "Unknown value type for config: #{name}"
-    end
+# Converts the given value to a Google::Protobuf::Value based on the specified value type.
+#
+# @param name [String, Symbol] the name of the config parameter
+# @param value [Object] the value to be converted
+# @param value_types [Hash] a hash mapping config parameter names to their value types (:number or :string)
+# @yield [Object] an optional block to transform the value before conversion
+# @return [Google::Protobuf::Value] the converted Google::Protobuf::Value
+# @raise [InvalidConfigError] if the value type is unknown
+def convert_value(name, value, value_types)
+  value = yield(value) if block_given?
+
+  case value_types[name.to_sym]
+  when :number
+    Google::Protobuf::Value.new(number_value: value)
+  when :string
+    Google::Protobuf::Value.new(string_value: value)
+  else
+    raise InvalidConfigError, "Unknown value type for config: #{name}"
   end
+end
 end
