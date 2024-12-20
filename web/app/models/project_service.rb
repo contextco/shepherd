3a48a3b3db242ab1cde2e897cf7a4fa4a4d75a85
alecbarber <alec@context.ai>
2024-12-20 17:24:09 +0000

    add in fully qualified url for consistency.

--- web/app/models/project_service.rb
+++ web/app/models/project_service.rb
@@ -25,7 +25,7 @@ class ProjectService < ApplicationRecord
     return [] if ports.blank?
 
     ports.map do |port|
-      "http://#{project.name}-#{name}-service:#{port}"
+      "http://#{project.name}-#{name}-service.#{project.name}.svc.cluster.local:#{port}"
     end
   end
 
