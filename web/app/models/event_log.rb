dd6ef8adc399d36ab596262279863d5b2589cd9b
Alex Gamble <agamble.uk@gmail.com>
2025-01-03 17:07:12 +0000

    Add health of deployments to the deashboard
    Former-commit-id: e40dcefce62f0265b0246b25f8219f166368f995

--- web/app/models/event_log.rb
+++ web/app/models/event_log.rb
@@ -2,4 +2,6 @@ class EventLog < ApplicationRecord
   belongs_to :agent_instance
 
   enum :event_type, { heartbeat: 0 }
+
+  after_create_commit -> { agent_instance.update!(last_heartbeat_at: Time.now) }, if: :heartbeat?
 end
