20251078290d233bc5da96f42088270d864db570
Alex Gamble <agamble.uk@gmail.com>
2025-01-06 12:02:56 +0000

    Add maintenance tasks
    Former-commit-id: 9cda17cabd74ed6990e6d550379e6b35cccad567

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_03_163824) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_06_120160) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -84,6 +84,27 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_03_163824) do
     t.datetime "updated_at", null: false
     t.uuid "helm_repo_id", null: false
     t.index ["helm_repo_id"], name: "index_helm_users_on_helm_repo_id"
+  end
+
+  create_table "maintenance_tasks_runs", force: :cascade do |t|
+    t.string "task_name", null: false
+    t.datetime "started_at", precision: nil
+    t.datetime "ended_at", precision: nil
+    t.float "time_running", default: 0.0, null: false
+    t.bigint "tick_count", default: 0, null: false
+    t.bigint "tick_total"
+    t.string "job_id"
+    t.string "cursor"
+    t.string "status", default: "enqueued", null: false
+    t.string "error_class"
+    t.string "error_message"
+    t.text "backtrace"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "arguments"
+    t.integer "lock_version", default: 0, null: false
+    t.text "metadata"
+    t.index ["task_name", "status", "created_at"], name: "index_maintenance_tasks_runs", order: { created_at: :desc }
   end
 
   create_table "project_services", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
