21d1044a5135a1784c9bb0b40f8dcc766d047b45
Alex Gamble <agamble.uk@gmail.com>
2025-01-08 15:06:32 +0000

    Significantly improve integration test setup
    Former-commit-id: 19aae8bd1ad188e0d0172fa8a90414971b30c32b

--- web/spec/integration/upgrades_spec.rb
+++ web/spec/integration/upgrades_spec.rb
@@ -5,15 +5,20 @@ require 'rails_helper'
 RSpec.describe 'Upgrades' do
   include ClusterHelpers
 
+  # If you are seeing strange behaviour in this test, this line may be a good starting point as it has some rough edges:
+  # https://github.com/rspec/rspec-rails/issues/2598#issuecomment-1109445577
+  uses_transaction "requires outside interference"
+
   let(:subscriber) { create(:project_subscriber) }
 
   around do |example|
     release_name = install_agent(subscriber)
+    wait_for_agent_to_come_online(subscriber)
     example.run
     uninstall_release(release_name)
   end
 
   it 'upgrades a subscriber' do
-    # Hooray!
+    Rails.logger.info("subscriber: #{subscriber.inspect}")
   end
 end
