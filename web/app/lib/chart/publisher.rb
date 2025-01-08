7aca51b600648476770ac2b91bea4ff6aece4fac
Alex Gamble <agamble.uk@gmail.com>
2025-01-08 09:59:14 +0000

    Add upgrades integration test
    Former-commit-id: a5b8b8317b07932617e8f72131acc02e67a93198

--- web/app/lib/chart/publisher.rb
+++ web/app/lib/chart/publisher.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Chart::Publisher
+  include AgentProto
   class ChartValidationError < StandardError; end
 
   def initialize(version, subscriber, helm_repos)
@@ -34,7 +35,7 @@ class Chart::Publisher
 
   def rpc_chart
     services_params = rpc_services
-    services_params << agent_rpc_service(subscriber) if version.full_agent?
+    services_params << agent_proto_definition(subscriber) if version.full_agent?
 
     Sidecar::ChartParams.new(
       name: project.name,
@@ -161,39 +162,5 @@ class Chart::Publisher
 
   def rpc_dependencies
     version.dependencies.map { |dep| rpc_dependency(dep) }
-  end
-
-  def agent_rpc_service(project_subscriber)
-    Sidecar::ServiceParams.new(
-      name: "shepherd-agent",
-      replica_count: 1,
-      image: Sidecar::Image.new(
-        name: "ghcr.io/contextco/onprem",
-        tag: "master",
-        pull_policy: Sidecar::ImagePullPolicy::IMAGE_PULL_POLICY_ALWAYS
-      ),
-      resources: Sidecar::Resources.new(
-        cpu_cores_requested: 1,
-        cpu_cores_limit: 1,
-        memory_bytes_requested: 2.gigabytes,
-        memory_bytes_limit: 2.gigabytes
-      ),
-      environment_config: Sidecar::EnvironmentConfig.new(
-        environment_variables: [
-          Sidecar::EnvironmentVariable.new(
-            name: "NAME",
-            value: project_subscriber.name
-          ),
-          Sidecar::EnvironmentVariable.new(
-            name: "BEARER_TOKEN",
-            value: project_subscriber.tokens.first.token
-          ),
-          Sidecar::EnvironmentVariable.new(
-            name: "BACKEND_ADDR",
-            value: ENV["SHEPHERD_AGENT_API_ENDPOINT"] || "https://agent.trustshepherd.com"
-          )
-        ]
-      )
-    )
   end
 end
---
+++ web/spec/support/cluster_helpers.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module ClusterHelpers
+  include AgentProto
+
+  def install_agent(subscriber)
+    req = Sidecar::GenerateAndInstallRequest.new(
+      chart: Sidecar::ChartParams.new(
+        name: 'agent-test-service',
+        version: '0.1.0',
+        services: [agent_proto_definition(subscriber)]
+      )
+    )
+
+    response = client.send(:generate_and_install, req)
+
+    response.release_name
+  end
+
+  def uninstall_release(release_name)
+    client.send(:uninstall, Sidecar::UninstallRequest.new(release_name: release_name))
+  end
+
+  def client
+    ::Gruf::Client.new(service: Sidecar::SidecarTest)
+  end
+end
