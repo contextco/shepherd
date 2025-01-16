5dd1b29dedb105243e629e8fcc20ba60df513962
alecbarber <alec@context.ai>
2025-01-16 13:49:43 +0000

    Make edit page for services viewable after deployment
    I have made all the fields disabled.
    
    
    Former-commit-id: 4a0e4d09b353fcced624048efadd28a4917abd91

--- web/app/components/services/disk_slider_component.rb
+++ web/app/components/services/disk_slider_component.rb
@@ -2,6 +2,7 @@
 
 class Services::DiskSliderComponent < ApplicationComponent
   attribute :form
+  attribute :disabled, default: false
 
   DEFAULT_OPTIONS = [
     1.gigabytes, 5.gigabytes, 10.gigabytes, 50.gigabytes, 100.gigabytes, 500.gigabytes, 1.terabyte, 5.terabytes
